866721e3333dceb401559a53a7a1525ae9e848be
nanny-giovanni <>
2024-09-11 17:26:04 -0300

    Aula   15

---
+++ Aula 15/script.js
@@ -0,0 +1,15 @@
+const SENHA_ELEMENTO = document.querySelector('#password')
+const VISOR_ELEMENTO = document.querySelector('.password-display')
+const TAMANHO_ELEMENTO_ELEMENTO = document.querySelector('#tamanho')
+const MAIUSCULA_ELEMENTO_ELEMENTO = document.querySelector('#maiusculas')
+const MINUSCULA_ELEMENTO = document.querySelector('#minusculas')
+const NUMEROS_ELEMENTO = document.querySelector('#numeros')
+const CARACTERES_ELEMENTO = document.querySelector('#caracteres')
+const GERAR_ELEMENTO = document.querySelector('.submit')
+const LIMPAR_ELEMENTO = document.querySelector('.reset')
+
+
+const MAIUSCULAS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+const MINUSCULAS = MAIUSCULAS.toLowerCase()
+const NUMEROS = '0123456789'
+const caracteres = '!@#$R()!$%¨&*<>1++Ç'
